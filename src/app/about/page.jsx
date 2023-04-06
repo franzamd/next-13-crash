880db2745d0709a90d41f90d0103417608a34894
Franz Medrano <franz.medrano@icloud.com>
2023-04-06 12:43:27 -0400

    initial commit

---
+++ src/app/about/page.jsx
@@ -0,0 +1,21 @@
+export const metadata = {
+  title: 'About Traversy Media',
+};
+
+const AboutPage = () => {
+  return (
+    <div>
+      <h1>About Traversy Media</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse aut
+        doloribus soluta quibusdam deleniti ea tempore molestiae consectetur,
+        quod, magnam iste modi maiores. Laborum accusamus fugiat, sunt enim quia
+        repudiandae. Amet numquam dolor illo soluta quisquam culpa, ullam vero
+        doloremque rem commodi dolorem animi voluptate! Deleniti ipsa
+        perspiciatis nemo itaque.
+      </p>
+    </div>
+  );
+};
+
+export default AboutPage;
---
+++ src/app/about/team/page.jsx
@@ -0,0 +1,14 @@
+const TeamPage = () => {
+  return (
+    <div>
+      <h1>Our Team</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel suscipit
+        magni molestias distinctio quis! Dicta voluptate neque voluptatibus
+        dolor corporis.
+      </p>
+    </div>
+  );
+};
+
+export default TeamPage;
